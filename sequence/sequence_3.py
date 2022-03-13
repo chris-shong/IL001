05dc437257e2bec26864eebdd47e531c54328ba1
SiyoungClee <csylee94@gmail.com>
2022-03-13 16:15:12 +0900

    review sequence #3

---
+++ sequence/sequence_3.py
@@ -0,0 +1,43 @@
+# HashTable: Key에 Value를 저장하는 구조
+# Python hashtable example: dict
+# key 값을 hashing function -> hash address -> key에 대한 value 참조
+
+# Dict 구조
+# print(__builtins__.__dict__)
+
+# Hash 값 확인
+t1 = (10, 20, (30, 40, 50))
+t2 = (10, 20, [30, 40, 50])
+
+print(hash(t1))
+# print(hash(t2))  # error -> mutable
+
+# Dict Setdefault 예제
+source = (('k1', 'val1'),
+          ('k1', 'val2'),
+          ('k2', 'val3'),
+          ('k2', 'val4'),
+          ('k2', 'val5'))
+
+new_dict1 = {}
+new_dict2 = {}
+
+# No use Setdefault
+for k, v in source:
+    if k in new_dict1:
+        new_dict1[k].append(v)
+    else:
+        new_dict1[k] = [v]
+
+print(new_dict1)
+
+# Use Setdefault
+for k, v in source:
+    new_dict2.setdefault(k, []).append(v)
+
+print(new_dict2)
+
+# 주의
+new_dict3 = {k: v for k, v in source}  # error -> 덮어쓰기
+
+print(new_dict3)
